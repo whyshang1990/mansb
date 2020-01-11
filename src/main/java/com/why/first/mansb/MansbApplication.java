8c23a054b30e2fa4c73801ce285a24e489242440
wuhaiyun <420133463@qq.com>
2020-01-11 14:09:42 +0800

    初始化

---
+++ src/main/java/com/why/first/mansb/MansbApplication.java
@@ -0,0 +1,13 @@
+package com.why.first.mansb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MansbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MansbApplication.class, args);
+	}
+
+}
