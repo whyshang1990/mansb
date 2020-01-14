8360b9404c1816cde943b5170d6c80cd1c376225
wuhaiyun <420133463@qq.com>
2020-01-14 15:16:05 +0800

    配置全局日志

--- src/main/java/com/why/first/mansb/Example.java
+++ src/main/java/com/why/first/mansb/Example.java
@@ -1,12 +1,24 @@
 package com.why.first.mansb;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Example {
+    private static Logger LOGGER = LoggerFactory.getLogger(Example.class);
+
     @GetMapping("/home")
     public String home() {
+        LOGGER.debug("打印日志");
+        LOGGER.info("打印日志");
         return "你好, Spring Boot";
+    }
+
+    public static void main(String[] args) {
+        LOGGER.debug("this is debug");
+        LOGGER.info("this is info");
+        LOGGER.error("this is error");
     }
 }
