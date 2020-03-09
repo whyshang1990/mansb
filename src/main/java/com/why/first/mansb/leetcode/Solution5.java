c407c6bb10bffa0fc180a9b4c74ea2106db63dbe
wuhaiyun <420133463@qq.com>
2020-03-09 18:19:50 +0800

    优化5，使用动态区域存储已识别的回文字符串

--- src/main/java/com/why/first/mansb/leetcode/Solution5.java
+++ src/main/java/com/why/first/mansb/leetcode/Solution5.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class Solution5 {
     // 第一次解法，91/103 通过测试用例。超时，需要优化
-    public String longestPalindrome(String s) {
+    public String longestPalindrome1(String s) {
         int length = s.length();
         int max = 0;
         Map<Integer, String> map = new HashMap<>();
@@ -20,6 +20,9 @@ public class Solution5 {
                         map.put(max, sub);
                     }
                 }
+                if (max == length - i) {
+                    break;
+                }
             }
         }
         return map.get(max);
@@ -27,22 +30,36 @@ public class Solution5 {
 
     private boolean checkString(String s) {
         int length = s.length();
-        int sum = 0;
         for (int i = 0; i < length - i; i++) {
             if(s.charAt(i) != s.charAt(length - i - 1)) {
-                break;
-            }
-            if (i == length - i - 1) {
-                sum += 1;
-            } else {
-                sum += 2;
+                return false;
             }
         }
-        return sum == length;
+        return true;
     }
 
+    public String longestPalindrome(String s) {
+        int length = s.length();
+        int maxLen = 0;
+        String maxPal = "";
+        boolean[][] P = new boolean[length][length]; //存储子字符串是否为回文字符串，下标表字符串起始位置
+        for (int len = 1; len <= length; len++) { // 遍历长度
+            for (int start = 0; start < length; start++) {
+                int end = start + len - 1;
+                if (end >= length) //下标已经越界，结束本次循环
+                    break;
+                P[start][end] = (len == 1 || len == 2 || P[start + 1][end - 1]) && s.charAt(start) == s.charAt(end);
+                if (P[start][end] && len > maxLen) {
+                    maxPal = s.substring(start, end + 1);
+                }
+            }
+        }
+        return maxPal;
+    }
+
+
     public static void main(String[] args) {
-        String s  = "";
+        String s  = "atttaddd";
         Solution5 solution = new Solution5();
         String r = solution.longestPalindrome(s);
         System.out.println("result: " + r);
